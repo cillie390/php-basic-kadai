cb47008d24fefe614b9bfb63125fee79a81abde7
cillie390 <161738154+cillie390@users.noreply.github.com>
2024-05-15 23:19:07 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+  <?php
+  $personal_date = ['name' =>'onion','price'=>200,'weight'=>160];
+
+  print_r($personal_date)
+?>
+
+ </body>
+ 
+ </html>
